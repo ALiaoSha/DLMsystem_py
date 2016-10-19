5876858f3bfc6d4c6e6871840840463e984533b6
unknown <intern001@rivereast.local>
2016-10-19 13:16:12 +0800

    initial introduction for this project

---
+++ EM.py
@@ -0,0 +1,6 @@
+
+class EM(object):
+    """docstring for EM."""
+    def __init__(self, arg):
+        super(EM, self).__init__()
+        self.arg = arg
---
+++ dlm.py
@@ -0,0 +1,24 @@
+
+
+"""
+notes:
+"""
+
+class dlmPrams(object):
+    """docstring for dlmPrams."""
+    def __init__(self, arg):
+        super(dlmPrams, self).__init__()
+        self.arg = arg
+
+
+class dlm(dlmPrams):
+    """docstring for dlm."""
+    def __init__(self, arg):
+        super(dlm, self).__init__()
+        self.arg = arg
+
+class PredictionResult(object):
+    """docstring for PredictionResult."""
+    def __init__(self, arg):
+        super(PredictionResult, self).__init__()
+        self.arg = arg
